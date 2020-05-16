1b9867a958370f180a5b4e8e2ba32e43960a4314
Apoorba Bibeka <abibeka@kittelson.com>
2020-05-15 19:32:17 -0500

    Delete lines

--- wmatarawnav/parse_rawnav.py
+++ wmatarawnav/parse_rawnav.py
@@ -179,22 +179,6 @@ def clean_rawnav_data(DataDict, filename):
     returnDict = {'rawnavdata':rawnavdata,'SummaryData':SummaryData}
     return(returnDict)
      
-# TODO: Delete this function
-# def subset_rawnav_trip(CleanDataDict_, rawnav_inventory_filtered_, AnalysisRoutes_):
-#     '''
-#     Subset data for analysis routes
-#     '''
-#     FinDat = pd.DataFrame()
-#     SearchDF = rawnav_inventory_filtered_[['route','filename']].set_index('route')
-#     RouteFiles = (SearchDF.loc[AnalysisRoutes_,:].values).flatten()
-#     for file in RouteFiles:
-#         tempDf =CleanDataDict_[file]['rawnavdata']
-#         tempDf.loc[:,"filename"] = file
-#         FinDat = pd.concat([FinDat,tempDf])
-#     FinDat.reset_index(drop=True,inplace=True)
-#     FinDat = FinDat.query("route in @AnalysisRoutes_")
-#     return(FinDat)
-
 def subset_rawnav_trip1(RawnavDataDict_, rawnav_inventory_filtered_, AnalysisRoutes_):
     '''
     Subset data for analysis routes
