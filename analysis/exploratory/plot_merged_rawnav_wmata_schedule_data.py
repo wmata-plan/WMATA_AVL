ed6ce549416decd992b0cc34adebb3138eb86242
Apoorb <apoorb2510@gmail.com>
2020-06-15 21:26:21 -0500

    Revised method for merging stops data

--- analysis/exploratory/plot_merged_rawnav_wmata_schedule_data.py
+++ analysis/exploratory/plot_merged_rawnav_wmata_schedule_data.py
@@ -21,9 +21,9 @@ from datetime import datetime
 
 print(f"Run Section 1 Import Libraries and Set Global Parameters...")
 begin_time = datetime.now()
-import pandas as pd, os, sys, shutil
-import pyarrow.parquet as pq
-import pyarrow as pa
+import pandas as pd, os, sys, collections
+# from geopy.distance import geodesic
+from shapely.geometry import Point
 
 if not sys.warnoptions:
     import warnings
@@ -66,12 +66,10 @@ else:
 restrict_n = None
 q_jump_route_list = ['S1', 'S2', 'S4', 'S9', '70', '79', '64', 'G8', 'D32', 'H1', 'H2', 'H3', 'H4',
                      'H8','W47']  # 16 Gb RAM can't handle all these at one go
-analysis_routes = q_jump_route_list
 # analysis_routes = ['70', '64', 'D32', 'H8', 'S2']
-#analysis_routes = ['S1', 'S9', 'H4', 'G8', '64']
-# analysis_routes = ['G8']
-# analysis_routess = ['S2','S4','H1','H2','H3','79','W47']
-
+# analysis_routes = ['S1', 'S9', 'H4', 'G8', '64']
+#analysis_routes = ['S2','S4','H1','H2','H3','79','W47']
+analysis_routes = ['70']
 # 1.3 Import User-Defined Package
 ############################################
 import wmatarawnav as wr
@@ -83,9 +81,9 @@ print("*"*100)
 ########################################################################################################################
 print(f"Run Section 2 Analyze Route ---Subset RawNav Data...")
 begin_time = datetime.now()
-analysis_days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+analysis_days = ['Monday']
 day_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-assert(len(set(day_of_week)-set(analysis_days))>= 0), print("""
+assert(len(set(day_of_week)-set(analysis_days))>=0), print("""
                                                     analysis_days is a subset of following days: 
                                                     ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
                                                 """)
@@ -117,7 +115,8 @@ executionTime= str(datetime.now() - begin_time).split('.')[0]
 print(f"Run Time Section 2 Analyze Route ---Subset RawNav Data : {executionTime}")
 print("*"*100)
 
-# 3 Read, analyze and summarize GTFS Data
+
+# 3 Read, analyze and summarize wmata schedule Data
 ########################################################################################################################
 print(f"Run Section 3 Read, analyze and summarize WMATA schedule Data...")
 begin_time = datetime.now() ##
@@ -154,6 +153,7 @@ wmata_schedule_based_sum_dat= wr.include_wmata_schedule_based_summary(
     rawnav_sum_dat = rawnav_summary_dat,
     nearest_stop_dat =nearest_rawnav_point_to_wmata_schedule_correct_stop_order_dat
 )
+
 wmata_schedule_based_sum_dat.\
     set_index(['fullpath','filename','file_id','wday','start_date_time','end_date_time',
                'index_trip_start_in_clean_data','taglist','route_pattern','route','pattern'],inplace=True,drop=True)
@@ -165,14 +165,95 @@ wmata_schedule_based_sum_dat.to_excel(sum_out_file,merge_cells=False)
 
 # 3.7 Output GTFS+Rawnav Merged Files
 ############################################
-wmata_schedule_rawnav_out_file = os.path.join(path_processed_data,f'wmata_schedule_stop_locations_inventory.parquet')
-nearest_rawnav_point_to_wmata_schedule_correct_stop_order_dat.drop(columns='geometry',inplace=True)
-table_from_pandas = pa.Table.from_pandas(nearest_rawnav_point_to_wmata_schedule_correct_stop_order_dat)
-while os.path.isdir(wmata_schedule_rawnav_out_file):
-    shutil.rmtree(wmata_schedule_rawnav_out_file, ignore_errors=True)  # Remove data from RemFolder before writing
-pq.write_to_dataset(table_from_pandas, root_path= wmata_schedule_rawnav_out_file, \
-                    partition_cols=['filename','index_trip_start_in_clean_data'])
+# TODO: output files
+
 executionTime= str(datetime.now() - begin_time).split('.')[0]
 print(f"Run Time Section 3 Read, analyze and summarize GTFS Data : {executionTime}")
 print("*"*100)
 
+# 4 Plot Rawnav Trace and Nearest Stops
+###########################################################################################################################################################
+print(f"Run Section 4 Plot Rawnav Trace and Nearest Stops...")
+begin_time = datetime.now()  ##
+# 4.1 Add Summary data to Stop data from Plotting
+############################################
+subset_wmata_schedule_rawnav_dat = \
+    nearest_rawnav_point_to_wmata_schedule_correct_stop_order_dat[['filename', 'index_trip_start_in_clean_data',
+                                                                   'route_key','dist_from_previous_stop',
+                                                                   'pattern_name', 'stop_sort_order', 'index_loc',
+                                                                   'stop_lat', 'stop_lon','stop_heading',
+                                                                   'dist_nearest_point_from_stop', 'geometry',
+                                                                   'geo_description']]
+wmata_schedule_based_sum_dat.reset_index(inplace=True)
+subset_wmata_schedule_rawnav_dat = \
+    subset_wmata_schedule_rawnav_dat.merge(wmata_schedule_based_sum_dat,
+                                           on=['filename', 'index_trip_start_in_clean_data'],
+                                           how='left')
+subset_wmata_schedule_rawnav_dat = \
+    subset_wmata_schedule_rawnav_dat[
+        ['index_loc','filename', 'wday', 'start_date_time', 'end_date_time', 'route_pattern', 'route', 'pattern',
+         'stop_sort_order','geo_description','route_text_wmata_schedule','route_key','dist_from_previous_stop',
+         'stop_heading','stop_lat', 'stop_lon', 'index_trip_start_in_clean_data',
+         'index_trip_end_in_clean_data','dist_nearest_point_from_stop',
+         'start_odom_ft_wmata_schedule', 'end_odom_ft_wmata_schedule',
+         'trip_dist_mi_odom_and_wmata_schedule','trip_length_mi_direct_wmata_schedule',
+         'dist_odom_mi', 'crow_fly_dist_lat_long_mi',
+         'start_sec_wmata_schedule', 'end_sec_wmata_schedule',
+         'trip_dur_sec_wmata_schedule', 'trip_dur_from_sec', 'trip_duration_from_tags',
+         'start_lat_wmata_schedule', 'end_lat_wmata_schedule', 'start_long_wmata_schedule',
+         'end_long_wmata_schedule', 'dist_first_stop_wmata_schedule',
+         'trip_speed_mph_wmata_schedule', 'speed_odom_mph', 'speed_trip_tag_mph',
+         'sec_start', 'odom_ft_start', 'sec_end',
+         'odom_ft_end', 'lat_start', 'long_start', 'lat_end', 'long_end', 'geometry']]
+
+subset_wmata_schedule_rawnav_dat.rename(columns={'index_loc': 'closest_index_loc_in_rawnav'}, inplace=True)
+
+# 4.2 Plot Trajectories
+############################################
+subset_wmata_schedule_rawnav_dat.route = subset_wmata_schedule_rawnav_dat.route.astype('str')
+rawnav_qjump_dat.route = rawnav_qjump_dat.route.astype('str')
+
+group_rawnav_wmata_schedule = \
+    subset_wmata_schedule_rawnav_dat.groupby(['filename', 'index_trip_start_in_clean_data', 'route'])
+rawnav_qjump_dat = rawnav_qjump_dat.query("route in @analysis_routes")
+rawnav_groups = rawnav_qjump_dat.groupby(['filename', 'index_trip_start_in_clean_data', 'route'])
+
+tracker_usable_routes = collections.Counter()
+for name, grp in group_rawnav_wmata_schedule:
+    Pattern = grp["pattern"].values[0]
+    tracker_usable_routes[f"{name[2]}_{Pattern}"] += 1
+print(tracker_usable_routes)
+STOP = 2
+tracker = collections.Counter()
+
+len(rawnav_groups.groups.keys())
+
+for name, rawnav_grp in rawnav_groups:
+    pattern = rawnav_grp["pattern"].values[0]
+    if name in group_rawnav_wmata_schedule.groups:
+        relevant_rawnav_wmata_schedule_dat = group_rawnav_wmata_schedule.get_group(name)
+    else:
+        print(name)
+        continue
+    tracker[f"{name[2]}_{pattern}"] += 1
+    if tracker[f"{name[2]}_{pattern}"] >= STOP: continue
+    print(name, pattern)
+    wday = relevant_rawnav_wmata_schedule_dat["wday"].values[0]
+    hour = \
+        relevant_rawnav_wmata_schedule_dat.start_date_time.values[0].split(" ")[1].split(":")[0]
+    save_file = f"{wday}_{hour}_{name[2]}_{pattern}_{name[0]}_Row{int(name[1])}.html"
+    map1 = wr.plot_rawnav_trajectory_with_wmata_schedule_stops(
+        rawnav_grp, relevant_rawnav_wmata_schedule_dat)
+    save_dir_1 = os.path.join(path_processed_data, "TrajectoryFigures")
+    if not os.path.exists(save_dir_1): os.makedirs(save_dir_1)
+    save_dir_2 = os.path.join(save_dir_1, f'{name[2]}_{pattern}')
+    if not os.path.exists(save_dir_2): os.makedirs(save_dir_2)
+    save_dir_3 = os.path.join(path_processed_data, "TrajectoryFigures", f'{name[2]}_{pattern}', wday)
+    if not os.path.exists(save_dir_3): os.makedirs(save_dir_3)
+    map1.save(os.path.join(save_dir_3, f"{save_file}"))
+
+executionTime = str(datetime.now() - begin_time).split('.')[0]
+print(f"Run Time Section 4 Plot Rawnav Trace and Nearest Stops : {executionTime}")
+print("*" * 100)
+
+###########################################################################################################################################################
--- wmatarawnav/parse_wmata_schedule.py
+++ wmatarawnav/parse_wmata_schedule.py
@@ -15,8 +15,19 @@ import numpy as np
 import folium
 from folium import plugins
 
+
 # Eventually will clean the parse_rawnav.py functions to get these updated column names.
 def fix_rawnav_names(data):
+    """
+    Parameters
+    ----------
+    data: pd.DataFrame
+    data with mixed case and camel case column names.
+    Returns
+    -------
+    data: pd.DataFrame
+    data with snake_case format for column names
+    """
     if 'Unnamed: 0' in data.columns:
         data = data.drop(columns='Unnamed: 0')
     col_names = data.columns
@@ -24,7 +35,7 @@ def fix_rawnav_names(data):
     return data
 
 
-def merge_stops_gtfs_rawnav(wmata_schedule_dat, rawnav_dat):
+def merge_stops_wmata_schedule_rawnav(wmata_schedule_dat, rawnav_dat):
     '''
     Parameters
     ----------
@@ -41,25 +52,28 @@ def merge_stops_gtfs_rawnav(wmata_schedule_dat, rawnav_dat):
     geometry_stops = [Point(xy) for xy in zip(wmata_schedule_dat.stop_lon.astype(float),
                                               wmata_schedule_dat.stop_lat.astype(float))]
     geometry_points = [Point(xy) for xy in zip(rawnav_dat.long.astype(float), rawnav_dat.lat.astype(float))]
-    gd_wmata_schedule_dat =gpd.GeoDataFrame(wmata_schedule_dat, geometry=geometry_stops,crs={'init':'epsg:4326'})
-    gd_rawnav_dat =gpd.GeoDataFrame(rawnav_dat, geometry=geometry_points,crs={'init':'epsg:4326'})
+    gd_wmata_schedule_dat = gpd.GeoDataFrame(wmata_schedule_dat, geometry=geometry_stops, crs={'init': 'epsg:4326'})
+    gd_rawnav_dat = gpd.GeoDataFrame(rawnav_dat, geometry=geometry_points, crs={'init': 'epsg:4326'})
     # Project to 2-D plane
-    #https://gis.stackexchange.com/questions/293310/how-to-use-geoseries-distance-to-get-the-right-answer
-    gd_wmata_schedule_dat.to_crs(epsg=3310,inplace=True) # Distance in meters---Default is in degrees!
-    gd_rawnav_dat.to_crs(epsg=3310,inplace=True) # Distance in meters---Default is in degrees!
-    wmata_schedule_groups = gd_wmata_schedule_dat.groupby(['route','pattern']) # Group GTFS data
-    rawnav_groups = gd_rawnav_dat.groupby(['filename','index_trip_start_in_clean_data','route','pattern']) # Group rawnav data
-    nearest_rawnav_point_to_wmata_schedule_data =pd.DataFrame()
+    # https://gis.stackexchange.com/questions/293310/how-to-use-geoseries-distance-to-get-the-right-answer
+    gd_wmata_schedule_dat.to_crs(epsg=3310, inplace=True)  # Distance in meters---Default is in degrees!
+    gd_rawnav_dat.to_crs(epsg=3310, inplace=True)  # Distance in meters---Default is in degrees!
+    wmata_schedule_groups = gd_wmata_schedule_dat.groupby(['route', 'pattern'])  # Group GTFS data
+    rawnav_groups = gd_rawnav_dat.groupby(
+        ['filename', 'index_trip_start_in_clean_data', 'route', 'pattern'])  # Group rawnav data
+    nearest_rawnav_point_to_wmata_schedule_data = pd.DataFrame()
     for name, rawnav_group in rawnav_groups:
-        wmata_schedule_relevant_route_dat =\
-            wmata_schedule_groups.get_group((name[2],name[3])) #Get the relevant group in GTFS corresponding to rawnav.
+        # print(name)
+        wmata_schedule_relevant_route_dat = \
+            wmata_schedule_groups.get_group(
+                (name[2], name[3]))  # Get the relevant group in GTFS corresponding to rawnav.
         nearest_rawnav_point_to_wmata_schedule_data = \
             pd.concat([nearest_rawnav_point_to_wmata_schedule_data,
                        ckdnearest(wmata_schedule_relevant_route_dat, rawnav_group)])
-    nearest_rawnav_point_to_wmata_schedule_data.dist =\
-        nearest_rawnav_point_to_wmata_schedule_data.dist * 3.28084 # meters to feet
-    nearest_rawnav_point_to_wmata_schedule_data.Lat =\
-        nearest_rawnav_point_to_wmata_schedule_data.Lat.astype('float')
+    nearest_rawnav_point_to_wmata_schedule_data.dist = \
+        nearest_rawnav_point_to_wmata_schedule_data.dist * 3.28084  # meters to feet
+    nearest_rawnav_point_to_wmata_schedule_data.lat = \
+        nearest_rawnav_point_to_wmata_schedule_data.lat.astype('float')
     geometry_nearest_rawnav_point = []
     for xy in zip(nearest_rawnav_point_to_wmata_schedule_data.long,
                   nearest_rawnav_point_to_wmata_schedule_data.lat):
@@ -68,20 +82,17 @@ def merge_stops_gtfs_rawnav(wmata_schedule_dat, rawnav_dat):
     for xy in zip(nearest_rawnav_point_to_wmata_schedule_data.stop_lon,
                   nearest_rawnav_point_to_wmata_schedule_data.stop_lat):
         geometry_stop_on_route.append(Point(xy))
-    geometry = [LineString(list(xy)) for xy in zip(geometry_nearest_rawnav_point,geometry_stop_on_route)]
-    nearest_rawnav_point_to_wmata_schedule_data=\
-        gpd.GeoDataFrame(nearest_rawnav_point_to_wmata_schedule_data, geometry=geometry,crs={'init':'epsg:4326'})
-    nearest_rawnav_point_to_wmata_schedule_data.rename(columns = {'dist':'dist_nearest_point_from_stop'},inplace=True)
+    geometry = [LineString(list(xy)) for xy in zip(geometry_nearest_rawnav_point, geometry_stop_on_route)]
+    nearest_rawnav_point_to_wmata_schedule_data = \
+        gpd.GeoDataFrame(nearest_rawnav_point_to_wmata_schedule_data, geometry=geometry, crs={'init': 'epsg:4326'})
+    nearest_rawnav_point_to_wmata_schedule_data.rename(columns={'dist': 'dist_nearest_point_from_stop'}, inplace=True)
     return nearest_rawnav_point_to_wmata_schedule_data
 
-###################################################################################################################################
 
-# ckdnearest
-###################################################################################################################################
-#https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point-in-other-dataframe
 def ckdnearest(gdA, gdB):
-    #TODO: Write Documentation
+    # TODO: Write Documentation
     '''
+    # https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point-in-other-dataframe
     Parameters
     ----------
     gdA : TYPE
@@ -94,12 +105,191 @@ def ckdnearest(gdA, gdB):
     gdf : TYPE
         DESCRIPTION.
     '''
-    gdA.reset_index(inplace=True);gdB.reset_index(inplace=True)
-    nA = np.array(list(zip(gdA.geometry.x, gdA.geometry.y)) )
-    nB = np.array(list(zip(gdB.geometry.x, gdB.geometry.y)) )
+    gdA.reset_index(inplace=True, drop=True);
+    gdB.reset_index(inplace=True, drop=True)
+    nA = np.array(list(zip(gdA.geometry.x, gdA.geometry.y)))
+    nB = np.array(list(zip(gdB.geometry.x, gdB.geometry.y)))
     btree = cKDTree(nB)
     dist, idx = btree.query(nA, k=1)
     gdf = pd.concat(
-        [gdA.reset_index(drop=True), gdB.loc[idx, ['filename','IndexTripStartInCleanData','IndexLoc','Lat', 'Long']].reset_index(drop=True),
+        [gdA.reset_index(drop=True),
+         gdB.loc[idx, ['filename', 'index_trip_start_in_clean_data', 'index_loc', 'lat', 'long']].reset_index(
+             drop=True),
          pd.Series(dist, name='dist')], axis=1)
     return gdf
+
+
+def remove_stops_with_dist_over_100ft(nearest_rawnav_point_to_wmata_schedule_data_):
+    row_before = nearest_rawnav_point_to_wmata_schedule_data_.shape[0]
+    nearest_rawnav_point_to_wmata_schedule_data_ = \
+        nearest_rawnav_point_to_wmata_schedule_data_.query('dist_nearest_point_from_stop<100')
+    row_after = nearest_rawnav_point_to_wmata_schedule_data_.shape[0]
+    print(f'deleted {row_before-row_after} rows from {row_before} with distance to the nearest stop > 100 ft.')
+    return  nearest_rawnav_point_to_wmata_schedule_data_
+
+def assert_clean_stop_order_increase_with_odom(nearest_rawnav_point_to_wmata_schedule_data_):
+    row_before = nearest_rawnav_point_to_wmata_schedule_data_.shape[0]
+    nearest_rawnav_point_to_wmata_schedule_data_.\
+        sort_values(['filename','index_trip_start_in_clean_data','stop_sort_order'],inplace=True)
+    assert (nearest_rawnav_point_to_wmata_schedule_data_.duplicated(
+        ['filename', 'index_trip_start_in_clean_data','stop_sort_order']).sum() == 0)
+    while(sum(nearest_rawnav_point_to_wmata_schedule_data_.
+                groupby(['filename', 'index_trip_start_in_clean_data']).index_loc.diff().dropna() < 0) != 0):
+        nearest_rawnav_point_to_wmata_schedule_data_ = \
+            delete_rows_with_incorrect_stop_order(nearest_rawnav_point_to_wmata_schedule_data_)
+    row_after = nearest_rawnav_point_to_wmata_schedule_data_.shape[0]
+    print(f'deleted {row_before-row_after} from {row_before} stops with incorrect order')
+    return nearest_rawnav_point_to_wmata_schedule_data_
+
+def delete_rows_with_incorrect_stop_order(nearest_rawnav_point_to_wmata_schedule_data_):
+    '''
+    Keep deleting stops where the index location does not increase with stop order
+    :param nearest_rawnav_point_to_wmata_schedule_data_:
+    :return:
+    '''
+    nearest_rawnav_point_to_wmata_schedule_data_.loc[:, 'diff_index'] = \
+        nearest_rawnav_point_to_wmata_schedule_data_.groupby(['filename', 'index_trip_start_in_clean_data']). \
+            index_loc.diff().fillna(0)
+    wrong_snapping_dat = nearest_rawnav_point_to_wmata_schedule_data_.query('diff_index<0')
+    nearest_rawnav_point_to_wmata_schedule_data_ = nearest_rawnav_point_to_wmata_schedule_data_.query('diff_index>=0')
+    return(nearest_rawnav_point_to_wmata_schedule_data_)
+
+
+def include_wmata_schedule_based_summary(rawnav_q_dat, rawnav_sum_dat, nearest_stop_dat):
+    #TODO: Write Documentation
+    '''
+    Parameters
+    ----------
+    FinDat_ : TYPE
+        DESCRIPTION.
+    SumDat_ : TYPE
+        DESCRIPTION.
+    DatFirstLastStops_ :
+    Returns
+    -------
+    None.
+
+    '''
+    #5 Get summary after using GTFS data
+    ########################################################################################
+    first_last_stop_dat = get_first_last_stop_rawnav(nearest_stop_dat)
+    rawnav_q_stop_dat = \
+        rawnav_q_dat.merge(first_last_stop_dat,on=['filename','index_trip_start_in_clean_data'],how='right')
+    rawnav_q_stop_dat = rawnav_q_stop_dat.query('index_loc>=index_loc_first_stop & index_loc<=index_loc_last_stop')
+    rawnav_q_stop_dat = \
+        rawnav_q_stop_dat[['filename','index_trip_start_in_clean_data','lat','long','heading','odomt_ft','sec_past_st'
+                           ,'first_stop_dist_nearest_point','trip_length','route_text']]
+    Map1 = lambda x: max(x)-min(x)
+    rawnav_q_stop_sum_dat =\
+        rawnav_q_stop_dat.groupby(['filename','index_trip_start_in_clean_data']).\
+            agg({'odomt_ft':['min','max',Map1],
+                 'sec_past_st':['min','max',Map1],
+                 'lat':['first','last'],
+                 'long':['first','last'],
+                 'first_stop_dist_nearest_point':['first'],
+                 'trip_length':['first'],
+                 'route_text':['first']})
+    rawnav_q_stop_sum_dat.columns = ['start_odom_ft_wmata_schedule','end_odom_ft_wmata_schedule',
+                                     'trip_dist_mi_odom_and_wmata_schedule','start_sec_wmata_schedule',
+                                     'end_sec_wmata_schedule','trip_dur_sec_wmata_schedule',
+                                     'start_lat_wmata_schedule','end_lat_wmata_schedule',
+                                     'start_long_wmata_schedule','end_long_wmata_schedule',
+                                     'dist_first_stop_wmata_schedule','trip_length_mi_direct_wmata_schedule',
+                                     'route_text_wmata_schedule']
+    rawnav_q_stop_sum_dat.loc[:,['trip_dist_mi_odom_and_wmata_schedule']] =\
+        rawnav_q_stop_sum_dat.loc[:,['trip_dist_mi_odom_and_wmata_schedule']]/5280
+    rawnav_q_stop_sum_dat.loc[:,['trip_length_mi_direct_wmata_schedule']] =\
+        rawnav_q_stop_sum_dat.loc[:,['trip_length_mi_direct_wmata_schedule']]/5280
+    rawnav_q_stop_sum_dat.loc[:,'trip_speed_mph_wmata_schedule'] =\
+        round(3600*
+              rawnav_q_stop_sum_dat.trip_dist_mi_odom_and_wmata_schedule/
+              rawnav_q_stop_sum_dat.trip_dur_sec_wmata_schedule,2)
+    rawnav_q_stop_sum_dat.loc[:,['trip_dist_mi_odom_and_wmata_schedule','dist_first_stop_wmata_schedule',
+                                 'trip_length_mi_direct_wmata_schedule']] = \
+        round(rawnav_q_stop_sum_dat.loc[:,['trip_dist_mi_odom_and_wmata_schedule','dist_first_stop_wmata_schedule',
+                                           'trip_length_mi_direct_wmata_schedule']],2)
+    rawnav_q_stop_sum_dat = \
+        rawnav_q_stop_sum_dat.merge(rawnav_sum_dat,on=['filename','index_trip_start_in_clean_data'],how='left')
+    return rawnav_q_stop_sum_dat
+
+def get_first_last_stop_rawnav(nearest_rawnav_stop_dat):
+    last_stop_dat = nearest_rawnav_stop_dat.copy()
+    last_stop_dat.loc[:, "tempCol"] = \
+        last_stop_dat.groupby(['filename', 'index_trip_start_in_clean_data']).stop_sort_order.transform(max)
+    last_stop_dat = last_stop_dat.query('stop_sort_order==tempCol').reset_index(drop=True).drop(columns='tempCol')
+    last_stop_dat = last_stop_dat[['filename', 'index_trip_start_in_clean_data','index_loc',
+                                   'dist_nearest_point_from_stop']]
+    last_stop_dat.rename(columns={'index_loc': 'index_loc_last_stop',
+                                  'dist_nearest_point_from_stop': 'last_stop_dist_nearest_point'},inplace=True)
+    first_stop_dat = \
+        nearest_rawnav_stop_dat.groupby(['filename', 'index_trip_start_in_clean_data']).stop_sort_order.transform(min)
+    first_stop_dat = nearest_rawnav_stop_dat.copy()
+    first_stop_dat.loc[:, "tempCol"] = \
+        first_stop_dat.groupby(['filename', 'index_trip_start_in_clean_data']).stop_sort_order.transform(min)
+    first_stop_dat = first_stop_dat.query('stop_sort_order==tempCol').reset_index(drop=True).drop(columns='tempCol')
+    first_stop_dat.rename(columns={'index_loc': 'index_loc_first_stop',
+                                  'dist_nearest_point_from_stop': 'first_stop_dist_nearest_point'},inplace=True)
+    first_stop_dat.sort_values(['filename', 'index_trip_start_in_clean_data'], inplace=True)
+    first_last_stop_dat = first_stop_dat.merge(last_stop_dat,on=['filename','index_trip_start_in_clean_data'],how='left')
+    first_last_stop_dat.drop(columns=['geometry','lat','long','pattern','route'],inplace=True)
+    return first_last_stop_dat
+
+def plot_rawnav_trajectory_with_wmata_schedule_stops(rawnav_dat, wmata_schedule_stop_dat):
+    ## Link to Esri World Imagery service plus attribution
+    #https://www.esri.com/arcgis-blog/products/constituent-engagement/constituent-engagement/esri-world-imagery-in-openstreetmap/
+    esri_imagery = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+    esri_attribution = "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    this_map = folium.Map( tiles='cartodbdark_matter', zoom_start=16,max_zoom=25,control_scale=True)
+    folium.TileLayer(name="EsriImagery",tiles=esri_imagery, attr=esri_attribution,
+                     zoom_start=16,max_zoom=25,control_scale=True).add_to(this_map)
+    folium.TileLayer('cartodbpositron',zoom_start=16,max_zoom=20,control_scale=True).add_to(this_map)
+    folium.TileLayer('openstreetmap',zoom_start=16,max_zoom=20,control_scale=True).add_to(this_map)
+    fg = folium.FeatureGroup(name="Rawnav Trajectory")
+    this_map.add_child(fg)
+    line_grp = folium.FeatureGroup(name="WMATA Schedule Stops and Nearest Rawnav Point")
+    this_map.add_child(line_grp)
+    plot_marker_clusters(this_map, rawnav_dat,"lat","long",fg)
+    plot_lines_clusters(this_map, wmata_schedule_stop_dat, line_grp)
+    lat_longs = [[x,y] for x,y in zip(rawnav_dat.lat,rawnav_dat.long)]
+    this_map.fit_bounds(lat_longs)
+    folium.LayerControl(collapsed=True).add_to(this_map)
+    return(this_map)
+
+
+def plot_marker_clusters(this_map, dat, lat, long, feature_grp):
+    #TODO: Write Documentation
+    popup_field_list = list(dat.columns)
+    for i,row in dat.iterrows():
+        label = '<br>'.join([field + ': ' + str(row[field]) for field in popup_field_list])
+        #https://deparkes.co.uk/2019/02/27/folium-lines-and-markers/
+        folium.CircleMarker(
+                location=[row[lat], row[long]], radius= 2,
+                popup=folium.Popup(html = label,parse_html=False,max_width='200')).add_to(feature_grp)
+
+
+def plot_lines_clusters(this_map, dat, feature_grp):
+    #TODO: Write Documentation
+    '''
+    Parameters
+    ----------
+    this_map : TYPE
+        DESCRIPTION.
+    Dat : TYPE
+        DESCRIPTION.
+    FeatureGrp : TYPE
+        DESCRIPTION.
+    Returns
+    -------
+    None.
+    '''
+    popup_field_list = list(dat.columns)
+    popup_field_list.remove('geometry')
+    for i,row in dat.iterrows():
+        temp_grp = \
+            plugins.FeatureGroupSubGroup(feature_grp,f"{row.stop_sort_order}-{row.geo_description}-{row.pattern}")
+        this_map.add_child(temp_grp)
+        label = '<br>'.join([field + ': ' + str(row[field]) for field in popup_field_list])
+        #https://deparkes.co.uk/2019/02/27/folium-lines-and-markers/
+        line_points = [(tuples[1],tuples[0]) for tuples in list(row.geometry.coords)]
+        folium.PolyLine(line_points, color="red", weight=4, opacity=1\
+        ,popup=folium.Popup(html = label,parse_html=False,max_width='300')).add_to(temp_grp)
