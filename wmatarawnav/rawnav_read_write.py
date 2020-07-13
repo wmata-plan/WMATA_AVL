912801ba04f18fa834b31569b6be321d83d7afed
wytimmerman <wtimmerman@foursquareitp.com>
2020-07-13 04:40:35 -0400

    Updates read rawnav function to be generic to summary vs. raw data

--- wmatarawnav/rawnav_read_write.py
+++ wmatarawnav/rawnav_read_write.py
@@ -5,11 +5,14 @@ Created on Thu Jul  9 12:20:18 2020
 @author: WylieTimmerman, Apoorba Bibeka
 """
 
+import os
+import pandas as pd
+import pyarrow as pa
 import pyarrow.parquet as pq
 from . import low_level_fns as ll
 
 
-def read_cleaned_rawnav(analysis_routes_, path_processed_route_data, restrict, analysis_days_):
+def read_cleaned_rawnav(analysis_routes_, path, analysis_days_):
     """
     Parameters
     ----------
@@ -17,7 +20,7 @@ def read_cleaned_rawnav(analysis_routes_, path_processed_route_data, restrict, a
         routes for which rawnav data is needed. Should be a subset of following:
         ['S1', 'S2', 'S4', 'S9', '70', '79', '64', 'G8', 'D32', 'H1', 'H2', 'H3', 'H4',
                          'H8','W47']
-    path_processed_route_data: str,
+    path: str,
        path where the parquet files for cleaned data is kept
     restrict: None, if all rawnav files need to be searched
     analysis_days_: list,
@@ -47,12 +50,18 @@ def read_cleaned_rawnav(analysis_routes_, path_processed_route_data, restrict, a
     rawnav_dat = pd.DataFrame()
     
     for analysis_route in analysis_routes_:
+        # Note that forloop on analysis_routes and analysis days filter 
+        #  is effectively moot now based on how we iterate,
+        #  but leaving this stuff for now to add some degree of flexibility
         filter_parquet = [[('wday', '=', day)] for day in analysis_days_]
         try:
-            rawnav_temp_dat = \
-                pq.read_table(source=os.path.join(path_processed_route_data,
-                                                  f"Route{analysis_route}_Restrict{restrict}.parquet"),
-                              filters=filter_parquet).to_pandas()
+            rawnav_temp_dat = (
+                pq.read_table(source=os.path.join(path),
+                              filters=filter_parquet)
+                .to_pandas())
+            
+            # NOTE: could selectt fewer columns to save memory/time on read in, but don't expect
+            # performance boost to be that great, could be obnoxious
         except Exception as e:
             if str(type(e)) == "<class 'IndexError'>":
                 raise ValueError('No data found for given filter conditions')
@@ -60,78 +69,19 @@ def read_cleaned_rawnav(analysis_routes_, path_processed_route_data, restrict, a
                 print(e)
                 raise
         else:
-            # NOTE: some of this due to conversion to and from parquet, weird things can happen
-            rawnav_temp_dat.route = rawnav_temp_dat.route.astype('str')
-            rawnav_temp_dat.drop(columns=['Blank', 'LatRaw', 'LongRaw', 'SatCnt', '__index_level_0__'], 
-                                 inplace=True)
+            # Even after defining the schema on parquet write, we're still seeing some strings 
+            # read in as categories rather than as strings. Very odd.
+            rawnav_temp_dat.route = rawnav_temp_dat.route.astype('str') 
+            rawnav_temp_dat.wday = rawnav_temp_dat.wday.astype('str') 
+            # Even though we could store as int, in case the values have NA's, we store as float
+            # and then convert to int after the fact. If you happen to run into a problem here, do an adhoc
+            # load of the parquet file and then after filtering NA values, convert pattern to 
+            # integer.
+            rawnav_temp_dat.pattern = rawnav_temp_dat.pattern.astype('int') 
 
             rawnav_temp_list.append(rawnav_temp_dat)
     rawnav_dat = pd.concat(rawnav_temp_list)
     return rawnav_dat
 
-def read_summary_rawnav(analysis_routes_, path_processed_route_data, restrict, analysis_days_):
-    """
-    Parameters
-    ----------
-    analysis_routes_: list,
-        routes for which rawnav data is needed. Should be a subset of following:
-        ['S1', 'S2', 'S4', 'S9', '70', '79', '64', 'G8', 'D32', 'H1', 'H2', 'H3', 'H4',
-                         'H8','W47']
-    path_processed_route_data: str,
-       path where the parquet files for cleaned data is kept
-    restrict: None, if all rawnav files need to be searched
-    analysis_days_: list,
-        days of the week for which data is needed. Should be a subset of following:
-        ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-    Returns
-    -------
-    rawnav_summary_dat: pd.DataFrame
-        rawnav summary data
-    rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat: pd.DataFrame
-        rows that are removed from summary data.
-    """
-    # Parameter Checks
-    
-    # Convert to analysis_routes back to list if not already
-    analysis_routes_ = ll.check_convert_list(analysis_routes_)    
-    analysis_days_ = ll.check_convert_list(analysis_days_)
 
-
-    day_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    assert (set(analysis_days_).issubset(set(day_of_week))),\
-        print("""analysis_days_ is a subset of following days: 
-              ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
-              """)
-    assert (len(analysis_days_) == len(set(analysis_days_))),\
-        print("analysis_days_ entries cannot be duplicated")       
-
-    # Function Body
-    
-    rawnav_temp_list = []
-    rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_list = []
-    rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat = pd.DataFrame()
-    rawnav_summary_dat = pd.DataFrame()
- 
-    for analysis_route in analysis_routes_:
-        temp_rawnav_sum_dat = \
-            pd.read_csv(os.path.join(path_processed_route_data,
-                                     f'TripSummaries_Route{analysis_route}_Restrict{restrict}.csv'))
-        total_rows_read = temp_rawnav_sum_dat.shape[0]
-        temp_rawnav_sum_dat.IndexTripStartInCleanData = temp_rawnav_sum_dat.IndexTripStartInCleanData.astype('int32')
-        temp_rawnav_sum_dat = temp_rawnav_sum_dat.query('wday in @analysis_days_')
-        if temp_rawnav_sum_dat.shape[0] == 0:
-            raise ValueError(f"No trips on any of the analysis_days_ ({analysis_days_})")
-        temp_rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat = \
-            temp_rawnav_sum_dat.query('TripDurFromSec < 600 | DistOdomMi < 2')
-        temp_rawnav_sum_dat = temp_rawnav_sum_dat.query('not (TripDurFromSec < 600 | DistOdomMi < 2)')
-        rows_removed_in_above_query = temp_rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat.shape[0]
-        print(f'Removing {rows_removed_in_above_query} out of {total_rows_read} trips/ rows with TripDurFromSec < 600'
-              f' seconds or DistOdomMi < 2 miles from route {analysis_route}')
-        rawnav_temp_list.append(temp_rawnav_sum_dat)
-        rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_list.append(
-            temp_rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat)
-    rawnav_summary_dat = pd.concat(rawnav_temp_list)
-    rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat = \
-        pd.concat(rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_list)
-    return rawnav_summary_dat, rawnav_tripdur_less_than_600sec_dist_odom_less_than_2mi_dat
 
